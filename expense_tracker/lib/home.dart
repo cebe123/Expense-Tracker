e48ed645658c9219768cad6f47b498a6b43aa076
cebe123 <adnancebe@hotmail.com>
2024-03-13 02:51:38 +0300

    center card added

--- expense_tracker/lib/home.dart
+++ expense_tracker/lib/home.dart
@@ -6,47 +6,135 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(98, 177, 177, 177),
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+      bottomNavigationBar: _buildBottomBar(),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.grey[200],
+      toolbarHeight: 60,
+      title: _buildAppBarTitle(),
+    );
+  }
+
+  Widget _buildAppBarTitle() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        _buildAppBarTitleLeft(),
+        _buildAppBarTitleRight(),
+      ],
+    );
+  }
+
+  Widget _buildAppBarTitleLeft() {
+    return Row(
+      children: [
+        CircleAvatar(
+          backgroundColor: Colors.grey[300],
+          radius: 20.0,
+          child: Icon(
+            Icons.person_3_sharp,
+            color: Colors.grey[600],
+          ),
+        ),
+        const SizedBox(width: 10.0),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              children: [
-                CircleAvatar(
-                  backgroundColor: Color.fromARGB(210, 189, 189, 198),
-                  radius: 20,
-                  child: Icon(
-                    Icons.person_3_sharp,
-                    color: Color.fromRGBO(70, 70, 70, 0.573),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Welcome",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontSize: 14,
-                      ),
-                    ),
-                    Text(
-                      "User name",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+            Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            ),
+            const Text(
+              'Username', // Replace with dynamic username
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w500,
+              ),
             ),
           ],
         ),
-      ),
-      body: Container(),
+      ],
     );
   }
+
+  Widget _buildAppBarTitleRight() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        IconButton(
+          icon: const Icon(Icons.settings),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBody() {
+    return Center(
+      child: Column(
+        children: [
+          const SizedBox(height: 30),
+          Container(
+            height: 200,
+            width: 300,
+            constraints: const BoxConstraints(maxHeight: 200),
+            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+            margin: const EdgeInsets.only(
+              left: 10,
+              right: 10,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: const Color.fromRGBO(82, 78, 199, 0.937),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 5,
+                  spreadRadius: 0.5,
+                  offset: Offset(5, 5),
+                )
+              ],
+              gradient: const LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [
+                  Colors.purple,
+                  Colors.blue,
+                ],
+              ),
+            ),
+            child: const Text(
+              "Total Balance",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Roboto',
+                color: Colors.white,
+                shadows: [
+                  Shadow(
+                    color: Colors.black26,
+                    offset: Offset(1, 5),
+                    blurRadius: 3,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget _buildBottomBar() {
+  return const Text("data");
 }
