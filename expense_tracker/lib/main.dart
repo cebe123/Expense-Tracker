c22e56d712b86d058e1451fe76d7eb8b0d3b87ce
cebe123 <adnancebe@hotmail.com>
2024-03-12 07:29:36 +0300

    home file added

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:expense_tracker/settings_page.dart';
+import 'package:expense_tracker/home.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,57 +13,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(98, 177, 177, 177),
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                CircleAvatar(
-                  backgroundColor: Color.fromARGB(210, 189, 189, 198),
-                  radius: 20,
-                  child: Icon(
-                    Icons.person_3_sharp,
-                    color: Color.fromRGBO(70, 70, 70, 0.573),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Welcome",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontSize: 14,
-                      ),
-                    ),
-                    Text(
-                      "User name",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-      body: Container(),
     );
   }
 }
